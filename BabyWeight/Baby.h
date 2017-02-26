ccea1b1a8a405a3e89bd91f9eee73340d8413931
Daniel Verdugo <daniel.verdugo521@gmail.com>
2017-02-26 15:19:56 -0800

    Just trying different things out. Ignore.

--- BabyWeight/Baby.h
+++ BabyWeight/Baby.h
@@ -7,23 +7,34 @@ using namespace std;
 class Baby {
 public:
 	Baby() {  // default constructor
+		// Heyo
 	};
 
 	Baby(string s, int w) { // constructor
-		// TO BE COMPLETED
+		name = s;
+		weight = w;
+	}
+
+	Baby & operator=(Baby & b) // Copy constructor (WHY DO THEY CALL IT A COPY CONSTRUCTOR!!!) It a damn operator overload!!!
+	{
+		name = b.getName();
+		weight = b.getWeight();
+		return *this;
 	}
 
 	// a "getter" method
 	int getWeight() {
-		return -1; // TO BE COMPLETED
+		return weight; // TO BE COMPLETED
 	}
 
 	// a "getter" method
-	string getName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
+	const string & getName() {
+		return name; // TO BE COMPLETED
 	}
 
 private:
 	string name;
 	int weight;
+
+	friend class MedicalRecord;
 };
--- BabyWeight/MedicalRecord.h
+++ BabyWeight/MedicalRecord.h
@@ -10,12 +10,14 @@ class MedicalRecord {
 public:
 	// default constructor
 	MedicalRecord() {
-		// TO BE COMPLETED
+		babyList = new Baby[1000000];
+		count = 0;
+
 	}
 
 	// destructor
 	~MedicalRecord() {
-		// TO BE COMPLETED
+		delete[] babyList;
 	}
 
 	// Load information from a text file with the given filename.
@@ -39,31 +41,83 @@ public:
 
 	// return the most frequently appearing name in the text file
 	string mostPopularName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
+		string name[1000];	// The names
+		int countPerName[1000];	// The number of babies with name, parallel with the name[] array
+		for (int i = 1; i < 1000; i++)	// Starts at 1 because the first index (0) will have one name and baby initially
+		{
+			countPerName[i] = 0;
+		}
+		name[0] = babyList[0].getName();
+		countPerName[0] = 1;
+		int numOfNames = 1;		// Number of names in the array.
+		for (int i = 1; i < count; i++)
+		{
+			bool namePlaced = false;
+			for (int x = 0; x < numOfNames; x++)
+			{
+				if (babyList[i].getName() == name[x])
+				{
+					countPerName[x]++;
+					namePlaced = true;
+				}
+			}
+			if (namePlaced == false)
+			{
+				name[numOfNames] = babyList[i].getName();
+				countPerName[numOfNames] = 1;
+				numOfNames++;
+			}
+		}
+		int popNameIndex= 0;
+		int popNameCount = 0;
+		for (int i = 0; i < numOfNames; i++)
+		{
+			if (countPerName[i] > popNameCount)
+			{
+				popNameCount = countPerName[i];
+				popNameIndex = i;
+			}
+		}
+		return name[popNameIndex]; // TO BE COMPLETED
 	}
-
 	// return the number of baby records loaded from the text file
-	int numberOfBirths() {
-		return -1; // TO BE COMPLETED
+	int numberOfBirths() {												// Done
+		return count;
 	}
 
 	// return the number of babies who had birth weight < 2500 grams
-	int numberOfBabiesWithLowBirthWeight() {
-		return -1; // TO BE COMPLETED
+	int numberOfBabiesWithLowBirthWeight() {							// Done
+		int lowWeightCount = 0;
+		for (int i = 0; i < count; i++)
+		{
+			if (babyList[i].getWeight() < 2500)
+				lowWeightCount++;
+		}
+		return lowWeightCount;
 	}
 
 	// return the number of babies who have the name contained in string s
-	int numberOfBabiesWithName(string s) {
-		return -1; // TO BE COMPLETED
+	int numberOfBabiesWithName(string s) {									// Done
+		int numBabiesWithName = 0;
+		for (int i = 0; i < count; i++)
+		{
+			if (babyList[i].getName() == s)
+				numBabiesWithName++;
+		}
+		return numBabiesWithName;
 	}
 
 private:
 	// update the data structure with information contained in Baby object
-	void addEntry(Baby b) {
-		// TO BE COMPLETED
+	void addEntry(Baby b) {													// Done
+		babyList[count] = b;
+		count++;
 	}
 
 	// Add private member variables for your data structure along with any 
 	// other variables required to implement the public member functions
+	Baby * babyList;
+	int count;
+
 
 };
